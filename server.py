8f676da80d9d2ad878ac5855ca166788d6edf4cb
hrit99 <hritikpihal51299@gmail.com>
2024-12-19 07:12:09 +0530

    Modified postgres url

--- server.py
+++ server.py
@@ -27,7 +27,7 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 # Configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL').replace('postgres://', 'postgresql://')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 app.config['OAUTH_CREDENTIALS'] = {
